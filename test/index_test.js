ab4222bc83042772a597b9bdf39af0d2f15b6ec9
Jeff Long <hello@jeffreylong.org>
2014-12-28 01:46:27 -0600

    Restore newlines from CLI; add CLI tests

--- test/index_test.js
+++ test/index_test.js
@@ -1,8 +1,58 @@
-var assert = require('assertive');
-var squawk = require('../index');
+var assert = require('assertive'),
+    childProcess = require('child_process'),
+    squawk = require('../index');
 
+function squawkCli(testString, cb){
+  childProcess.exec('./bin/squawk ' + testString, function(err, stdout, stderr){
+    cb(err, stdout);
+  });
+}
 
 describe('squawk', function(){
+  describe('from the command line', function(){
+    it('returns each character on its own line', function(done){
+      var expected = '-\n-\n-\n';
+      squawkCli('---', function(err, actual){
+        assert.equal(expected, actual);
+        done();
+      });
+    });
+
+    it('replaces numbers with their word form', function(done){
+      var expected = 'two\nfour\n';
+      squawkCli('24', function(err, actual){
+        assert.equal(expected, actual);
+        done();
+      });
+    });
+
+    describe('letters', function(){
+      it('replaces letters with their word form', function(done){
+        var expected = 'alpha\ncharlie\n';
+        squawkCli('ac', function(err, actual){
+          assert.equal(expected, actual);
+          done();
+        });
+      });
+
+      it('handles mixed case', function(done){
+        var expected = 'oscar\nkilo\n';
+        squawkCli('Ok', function(err, actual){
+          assert.equal(expected, actual);
+          done();
+        });
+      });
+    });
+
+    it('handles a mix of letters and numbers', function(done){
+      var expected = 'mike\nfive\nvictor\ntwo\ntango\nsix\n';
+      squawkCli('M5V2T6', function(err, actual){
+        assert.equal(expected, actual);
+        done();
+      });
+    });
+  });
+
   describe('as a library', function(){
     it('exists', function(){
       assert.truthy(squawk);
