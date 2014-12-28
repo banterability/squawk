ca524966b4ab374a7cae0385698434ec18a76130
Jeff Long <hello@jeffreylong.org>
2014-12-28 00:59:00 -0600

    Extract number processing

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
-NUMBERS = {
-  '0': 'zero',
-  '1': 'one',
-  '2': 'two',
-  '3': 'three',
-  '4': 'four',
-  '5': 'five',
-  '6': 'six',
-  '7': 'seven',
-  '8': 'eight',
-  '9': 'nine'
-};
+var processNumbers = require('./lib/numbers');
 
 function isDigit(ch){
   return !!ch.match(/\d/);
 }
 
-function numericCharAsWord(char){
-  var numberWord;
-  if (numberWord = NUMBERS[char]){
-    return numberWord;
-  }
-  return char;
-}
-
 function processCharacter(ch){
   if(isDigit(ch)){
-    return numericCharAsWord(ch);
+    return processNumbers(ch);
   }
   return ch;
 }
