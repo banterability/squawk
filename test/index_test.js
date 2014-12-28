685d3910a419002798bbb39856fb6c6ed2ba566e
Jeff Long <hello@jeffreylong.org>
2014-12-28 01:46:00 -0600

    Nest library tests

--- test/index_test.js
+++ test/index_test.js
@@ -1,35 +1,38 @@
 var assert = require('assertive');
 var squawk = require('../index');
 
+
 describe('squawk', function(){
-  it('exists', function(){
-    assert.truthy(squawk);
-  });
-
-  it('returns each character on its own line', function(){
-    var expected = ['-', '-', '-'];
-    assert.deepEqual(expected, squawk('---'));
-  });
-
-  it('replaces numbers with their word form', function(){
-    var expected = ['two', 'four'];
-    assert.deepEqual(expected, squawk('24'));
-  });
-
-  describe('letters', function(){
-    it('replaces letters with their word form', function(){
-      var expected = ['alpha', 'charlie'];
-      assert.deepEqual(expected, squawk('ac'));
+  describe('as a library', function(){
+    it('exists', function(){
+      assert.truthy(squawk);
     });
 
-    it('handles mixed case', function(){
-    var expected = ['oscar', 'kilo'];
-    assert.deepEqual(expected, squawk('Ok'));
+    it('returns each character on its own line', function(){
+      var expected = ['-', '-', '-'];
+      assert.deepEqual(expected, squawk('---'));
     });
-  });
 
-  it('handles a mix of letters and numbers', function(){
-    var expected = ['mike', 'five', 'victor', 'two', 'tango', 'six'];
-    assert.deepEqual(expected, squawk('M5V2T6'));
+    it('replaces numbers with their word form', function(){
+      var expected = ['two', 'four'];
+      assert.deepEqual(expected, squawk('24'));
+    });
+
+    describe('letters', function(){
+      it('replaces letters with their word form', function(){
+        var expected = ['alpha', 'charlie'];
+        assert.deepEqual(expected, squawk('ac'));
+      });
+
+      it('handles mixed case', function(){
+        var expected = ['oscar', 'kilo'];
+        assert.deepEqual(expected, squawk('Ok'));
+      });
+    });
+
+    it('handles a mix of letters and numbers', function(){
+      var expected = ['mike', 'five', 'victor', 'two', 'tango', 'six'];
+      assert.deepEqual(expected, squawk('M5V2T6'));
+    });
   });
 });
